8e00bc854396bba5441f29599fd474723fdd5445
Suppachai <suppachai.g@ku.th>
2021-07-29 17:11:14 +0700

    create back-end

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a9b52d9cf208f7ed9366cd84c414435e'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '9776f306-84d4-4e26-a543-fb5d1224c717'
+};
